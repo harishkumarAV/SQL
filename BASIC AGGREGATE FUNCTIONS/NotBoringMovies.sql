99a5f78ad4ed57fb4ec292e8ce9fbc18beb06976
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2024-04-29 09:00:09 +0530

    Update NotBoringMovies.sql

--- BASIC AGGREGATE FUNCTIONS/NotBoringMovies.sql
+++ BASIC AGGREGATE FUNCTIONS/NotBoringMovies.sql
@@ -1,3 +1,11 @@
 # Write your MySQL query statement below
 select id , movie , description, rating from cinema where id%2 =1 && description!='boring'
 order by rating desc;
+
+
+the aggregate function that could be used here is mod().
+  
+SELECT * FROM Cinema
+WHERE mod(id,2) = 1 
+    AND description != 'boring'
+ORDER BY rating DESC;
